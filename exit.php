053f2b4f94802d1438f1384ee426ad9a59df533c
NthTryMan <111993380+qwetry4444@users.noreply.github.com>
2023-05-22 01:29:35 +0500

    db

---
+++ exit.php
@@ -0,0 +1,8 @@
+<?php
+function exit(){
+    session_start(["use_strict_mode" => true]);
+    $_SESSION['id'] = NULL;
+    $_SESSION['username'] = NULL;
+}
+
+?>
--- task.php
+++ task.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="css/style_header.css">
-    <link rel="stylesheet" href="css/style_task.css">
+    <link rel="stylesheet" href="css/style_tasks.css">
     <title>CodeDay Task</title>
 </head>
 
