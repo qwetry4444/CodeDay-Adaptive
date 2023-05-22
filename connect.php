f85d5ced90823ea6d9b4912860a817d239992c04
NthTryMan <111993380+qwetry4444@users.noreply.github.com>
2023-05-22 17:02:34 +0500

    Соединение с БД

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
         $password = '123';
         $conn_string = "host=localhost port=5434 dbname=CodeDay user=postgres password=123";
         $dbconn = pg_connect($conn_string);
-
+        
         return $dbconn;
     }
 ?>
