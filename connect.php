dc8b3050bfdcbe9372143791fa1d4bcc66fff8e8
NthTryMan <111993380+qwetry4444@users.noreply.github.com>
2023-06-24 08:49:14 +0500

    Подключение к БД

--- connect.php
+++ connect.php
@@ -1,13 +1,13 @@
 <?php
     function connect()
     {
-        session_start(["use_strict_mode" => true]);
         $host = 'localhost';
         $port = 5434;   
         $dbname = 'CodeDay';
         $username = 'postgres';
         $password = '123';
-        $conn_string = "host=localhost port=5434 dbname=CodeDay user=postgres password=123";
+        $conn_string = "host='%s' port=%d dbname='%s' user='%s' password=%d";
+        $conn_string = sprintf($conn_string, $host, $port, $dbname, $username, $password);
         $dbconn = pg_connect($conn_string);
         
         return $dbconn;
